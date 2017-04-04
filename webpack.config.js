4c1d60d48f5759bbcb4055011e7c8e3ea09b6c06
1Renat <rent1995@yandex.ru>
2017-04-04 19:23:41 +0300

    new

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+   devtool: 'inline-soure-map',
+   entry: {
+   	js: path.join(__dirname, 'client/client.js')//входящий файл
+   },
+   output: {
+   	path: path.join(__dirname, 'public'),//вывод
+   	filename: 'bundle.js'
+   },
+  
+   module: {
+   	loaders: [
+   	{test: /\.js$/, loader: 'babel-loader'}
+
+   	]
+   }
+}
